6df533c7319a60251b24b14127c4b70a3111b668
Steve Lasker <StevenLasker@Hotmail.com>
2016-09-20 02:29:13 -0700

    Calling Magic 8 Ball with questions

---
+++ src/Magic8Ball/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Magic8Ball
+{
+    public class Program
+    {
+        static Random _r = new Random();
+        public static void Main(string[] question)
+        {
+            EightBallAnswers answers = new EightBallAnswers();
+            int rInt = _r.Next(0, answers.Count() - 1);
+            string response;
+            if (question.Length == 0)
+            {
+                response = "You must ask a question to be provided an answer.";
+            }
+            else
+            {
+                response = string.Format("The answer to your question: '{0}' is {1}", question[0], answers[rInt]);
+            }
+            Console.WriteLine( response);
+        }
+    }
+}
